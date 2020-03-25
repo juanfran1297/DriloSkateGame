11a596685f37c69d1bdea9da8c2c3b1e79731784
Juan Francisco Guillén Navarro <jfran191297@gmail.com>
2020-03-25 14:16:02 +0100

    Drilo
    Se ha puesto a Drilo con un solo sprite, se ha puesto un fondo y se ha empezado con el movimiento del personaje.

---
+++ Drilo Skate/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 12.0f;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //Input
+        if(Input.GetButtonDown("Fire1"))
+        {
+
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //Movement
+        rb.velocity = new Vector2(speed, 0f);
+    }
+}
